c15e5de05ce79ae5596e0a4545d67713aa9f7a6a
ViperZer0 <11594471+ViperZer0@users.noreply.github.com>
2025-05-21 19:56:05 -0700

    Added syntax highlight and got the rest of styling working

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,9 @@
 import graphVizPlugin from "../eleventy-plugin-graphviz/dist/index.js";
+import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 
 export default async function(eleventyConfig) {
   eleventyConfig.addPlugin(graphVizPlugin, { format: "svg" });
+  eleventyConfig.addPlugin(syntaxHighlight);
   // Copy bundle.css to output directory
   eleventyConfig.addPassthroughCopy({"src/_includes/bundle.css": "bundle.css"});
   // Add sorted posts collection
