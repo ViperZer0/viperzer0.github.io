075c51fae1bfdcc4460279705bd5967336e18180
ViperZer0 <11594471+ViperZer0@users.noreply.github.com>
2025-05-22 23:35:48 -0700

    Added svg icons and added source files into repo.
    Even though they're embedded directly into the HTML, I like having the
    original files around.

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,6 @@
 import graphVizPlugin from "../eleventy-plugin-graphviz/dist/index.js";
 import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
-import markdownItCallouts from "markdown-it-callouts";
+import markdownItCallouts from "../markdown-it-callouts/main.js";
 
 export default async function(eleventyConfig) {
   eleventyConfig.addPlugin(graphVizPlugin, { format: "svg" });
@@ -10,8 +10,10 @@ export default async function(eleventyConfig) {
     calloutTitleElementType: "div",
     calloutSymbolElementType: "div",
     calloutSymbols: {
-      info: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1.5em" height="1.5em"><path fill="currentColor" d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2m0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8"/></svg>'
-    }
+      info: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1.5em" height="1.5em"><path fill="currentColor" d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2m0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8"/></svg>',
+      note: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1.5em" height="1.5em"><path fill="currentColor" d="M3 10h11v2H3zm0-2h11V6H3zm0 8h7v-2H3zm15.01-3.13l.71-.71a.996.996 0 0 1 1.41 0l.71.71c.39.39.39 1.02 0 1.41l-.71.71zm-.71.71l-5.3 5.3V21h2.12l5.3-5.3z"/></svg>'
+    },
+    enableCalloutSymbolWithEmptyType: "callout-type"
   }));
 
 
